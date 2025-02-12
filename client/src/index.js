5b192ca11d27e59c3066caedfa2e1f72bfe0218f
Marvin van Kalsbeek <marvin@stacket.nl>
2025-02-12 19:40:52 -0400

    add react query provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+const queryClient = new QueryClient();
+
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
-root.render(<App />, document.getElementById("root"));
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
