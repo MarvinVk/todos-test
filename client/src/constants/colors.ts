fa2c95d021663e5031fec8debe98811d2f24f992
Marvin van Kalsbeek <marvin@stacket.nl>
2025-02-13 22:32:47 -0400

    change the constants files to typescript

---
+++ client/src/constants/colors.ts
@@ -0,0 +1,28 @@
+export enum colors {
+  white = '#ffffff',
+  neutral50 = '#fafafa',
+  neutral100 = '#f5f5f5',
+  neutral200 = '#e5e5e5',
+  neutral300 = '#d4d4d4',
+  neutral400 = '#a3a3a3',
+  neutral500 = '#737373',
+  neutral600 = '#525252',
+  neutral700 = '#404040',
+  neutral800 = '#262626',
+  neutral900 = '#171717',
+  neutral950 = '#0a0a0a',
+
+  red = '#FF3333',
+  redLight = '#FFE0E0',
+
+  green = '#009900',
+  greenLight = '#D9F6D9',
+
+  blue = '#0077CC',
+  blueLight = '#D9EBF8',
+
+  orange = '#FF9900',
+  orangeLight = '#FDEBD9',
+}
+
+export default colors;
