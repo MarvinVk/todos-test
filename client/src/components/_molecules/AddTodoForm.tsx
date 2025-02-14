e27f41ae5e71f4e776a524204a06a178350739ed
Marvin van Kalsbeek <marvin@stacket.nl>
2025-02-13 22:26:30 -0400

    updated styles of add todo form

--- client/src/components/_molecules/AddTodoForm.tsx
+++ client/src/components/_molecules/AddTodoForm.tsx
@@ -1,30 +1,70 @@
-import { FormEventHandler, useState } from 'react';
+import { FC, FormEventHandler, useContext, useState } from 'react';
+import styled from 'styled-components';
 
+import colors from '../../constants/colors';
+import sizes from '../../constants/sizes';
+import { TodoContext } from '../../context/todos';
 import { addTodo } from '../../services/todos';
 import { Button, Input } from '../_atoms';
 
-export const AddTodoForm = () => {
+const Styled = {
+  Component: styled.div`
+    position: relative;
+    bottom: 0;
+    width: 100%;
+    padding: ${sizes.space}px;
+    border-radius: 10px;
+    border-color: ${colors.neutral800};
+    background-color: ${colors.neutral200};
+  `,
+  FormHolder: styled.div`
+    display: flex;
+    width: 100%;
+  `,
+  Title: styled.h4`
+    font-weight: 600;
+    font-size: 16px;
+    margin: 0 0 ${sizes.space / 2}px;
+  `,
+  InputHolder: styled.div`
+    width: 100%;
+    margin-right: ${sizes.space / 2}px;
+  `,
+};
+
+type AddTodoFormProps = {
+  columnId: number;
+};
+
+export const AddTodoForm: FC<AddTodoFormProps> = ({ columnId }) => {
+  const { addCard } = useContext(TodoContext);
+
   const [title, setTitle] = useState('');
 
   const handleSubmit: FormEventHandler = async (e) => {
     e.preventDefault();
 
     const response = await addTodo(title, 1);
-    console.log('response :>> ', response);
+    addCard(columnId, response);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h3>Add todo:</h3>
-      <Input
-        type="text"
-        name="title"
-        placeholder="title (string)"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-
-      <Button type="submit">Submit</Button>
-    </form>
+    <Styled.Component>
+      <form onSubmit={handleSubmit}>
+        <Styled.Title>Add new todo</Styled.Title>
+        <Styled.FormHolder>
+          <Styled.InputHolder>
+            <Input
+              type="text"
+              name="title"
+              placeholder="Enter a title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Styled.InputHolder>
+          <Button type="submit">Submit</Button>
+        </Styled.FormHolder>
+      </form>
+    </Styled.Component>
   );
 };
